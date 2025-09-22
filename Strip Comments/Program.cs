7b0e040fa4dc9991830bf09739e9a2f18d4171bb
Icaro Martins dos Santos <132224296+icaromsantos@users.noreply.github.com>
2025-09-22 16:28:52 +0000

    primeiro commit

---
+++ Strip Comments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine(StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+            Console.WriteLine(StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+        }
+
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+
+            
+
+            return "";
+        }
+    }
+}
